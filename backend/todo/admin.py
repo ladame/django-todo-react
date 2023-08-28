19192fae3be61313736c6953d0373268b7f83061
ladame <salegrandmont@gmail.com>
2023-08-28 18:40:31 +0200

    Initial commit

---
+++ backend/todo/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Todo
+
+# root/root
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'gambaran', 'completed')
+
+# Register your models here.
+
+admin.site.register(Todo, TodoAdmin)
